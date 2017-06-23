4e3dbf3d07f27d10549ed3c75d0fa7e9f92351f0
Anton Kazarinov <askazarinov@gmail.com>
2017-06-24 00:53:21 +0500

    Настройка модели User
    Класс User реализует интерфейс IdentifyInterface и RateLimitInterface
    Для идентификации пользователя и ограничение количества запросов соответственно

--- models/User.php
+++ models/User.php
@@ -2,103 +2,77 @@
 
 namespace app\models;
 
-class User extends \yii\base\Object implements \yii\web\IdentityInterface
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use yii\filters\RateLimitInterface;
+
+/**
+ * @property integer $user_id
+ * @property string $username
+ * @property string $password_hash
+ * @property string $access_token
+ * @property string $allowance
+ * @property string $allowance_updated_at
+ */
+class User extends ActiveRecord implements IdentityInterface, RateLimitInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
     /**
-     * @inheritdoc
-     */
-    public static function findIdentity($id)
-    {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Finds user by username
+     * Number of requests per second
      *
-     * @param string $username
-     * @return static|null
+     * @var integer
      */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
+    private $rateLimit = 10;
 
-        return null;
+    public static function tableName()
+    {
+        return '{{%user}}';
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
     }
+
+    public function getAuthKey(): string
+    {
+        return null;
+    }
+
+    public function getId()
+    {
+        return $this->user_id;
+    }
+
+    public function validateAuthKey($authKey): bool
+    {
+        return false;
+    }
+
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::findOne(['access_token' => $token]);
+    }
+
+    public function getRateLimit($request, $action): array
+    {
+        return [$this->rateLimit, 1];
+    }
+
+    public function loadAllowance($request, $action): array
+    {
+        return [$this->allowance, $this->allowance_updated_at];
+    }
+
+    public function saveAllowance($request, $action, $allowance, $timestamp)
+    {
+        $this->allowance = $allowance;
+        $this->allowance_updated_at = $timestamp;
+        $this->save(false);
+    }
+
 }
